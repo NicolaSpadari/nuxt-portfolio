b4de437e2a9501c54c2f52100f881f5c4ba5d262
NicolaSpadari <spadari.nicola@gmail.com>
2022-09-09 11:44:08 +0200

    disable tracking

--- middleware/tracker.global.ts
+++ middleware/tracker.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-    if (!process.dev) {
+    if (false) {
         const location = await useLocation();
         const { sendMessage } = useMessage();
 
